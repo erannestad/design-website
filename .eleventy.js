7cacd71c071eb39db2e59c8de4f86d2ebc133693
Eric Rannestad <ericrannestad@gmail.com>
2023-03-29 17:29:57 -0600

    styles.css

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.setServerOptions({
-    watch: ['./public/css/styles.css']
+    watch: ['./docs/css/styles.css']
   });
 
   return {
