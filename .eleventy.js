ec4c3e6442bff502a42f34b4b059f57fb984f133
Eric Rannestad <ericrannestad@gmail.com>
2023-03-29 22:22:14 -0600

    url

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,13 @@ module.exports = function(eleventyConfig) {
 
   return {
     htmlTemplateEngine: "njk",
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/design/",
+
     dir: {
       input: 'src',
       output: 'docs'
